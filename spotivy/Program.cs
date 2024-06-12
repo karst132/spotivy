b85b7b1f7cee39fd77229e60f698e465bb53f7ca
Roverfloats <tygoliebers@gmail.com>
2024-06-12 10:24:33 +0200

    basic planning

--- spotivy/Program.cs
+++ spotivy/Program.cs
@@ -4,12 +4,35 @@
     {
         static async Task Main(string[] args)
         {
-            List<User> users = new List<User>();
-            List<Album> albums = new List<Album>();
-            List<Artist> artists = new List<Artist>();
-            List<Song> songs = new List<Song>();
+            Client client = new();
+            bool running = true;
+            while (running) {
+                Console.WriteLine("Run program by typing \"run\".");
+                Console.WriteLine("clear console by typing \"clear\".");
+                Console.WriteLine("Close the application by typing \"end\".");
+                string input;
+                input = Console.ReadLine().ToLower();
 
-            SongCollection currentPlay;
+                if (input == "run")
+                {
+                    client.RunProgram();
+                }
+                else if (input == "end")
+                {
+                    running = false;
+                    Console.WriteLine("Closing...");
+                    Console.WriteLine("");
+                }
+                else if (input == "clear")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine("");
+                }
+            }
         }
     }
 }
---
+++ spotivy/client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotivy
+{
+    internal class Client
+    {
+        List<User> users = new List<User>();
+        List<Album> albums = new List<Album>();
+        List<Artist> artists = new List<Artist>();
+        List<Song> songs = new List<Song>();
+
+        SongCollection currentPlay;
+
+        public void RunProgram()
+        {
+            // play song
+            // pause and start
+            // repeat song
+            // skip song
+            // stop song
+
+            // play playlist
+            // skip to next
+            // pause song
+            // stop playlist
+            // repeat playlist
+
+            // play album
+            // skip to next
+            // pause song
+            // stop album
+            // repeat album
+
+            // search all
+            // search songs
+            // search users
+            // search albums
+
+            // view user
+            // view user's playlists
+            // play one of user's playlists
+            // stop playlist
+            // view own playlists
+            // copy playlist
+            // view own playlists
+            // friend user
+            // unfriend user
+
+            // view own playlists
+            // create playlist (name "hello world")
+            // view own playlists
+            // add songs
+            // add another playlist to previously created one
+            // add album to previously created one
+            // play playlist
+            // stop playlist
+            // delete playlist
+        }
+    }
+}
